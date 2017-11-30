b0d674b6a25b017b002378704bf609f8f779ccd2
CaldasAnthony <33262464+CaldasAnthony@users.noreply.github.com>
2017-11-30 15:59:49 +0100

    Add files via upload

--- Tool_3D_linear_duoT_real.py
+++ Tool_3D_linear_duoT_real.py
@@ -357,9 +357,9 @@ if Profil == True :
             lim_alt = h
         save_adress = "/data1/caldas/Pytmosph3R/Tools/%s_real_npy/"%(name_exo)
         if Composition == False :
-            save_name_3D = "%s%s_3D_duo_linear_real_%i_%i_%i"%(save_adress,name_exo,np.amin(T_iso_array),np.amax(T_iso_array),beta,P_tau/(1.e+5))
+            save_name_3D = "%s%s_3D_duo_linear_real_%i_%i_%i_.2f"%(save_adress,name_exo,np.amin(T_iso_array),np.amax(T_iso_array),beta,P_tau/(1.e+5))
         else :
-            save_name_3D = "%s%s_3D_duo_linear_real_%i_%i_%i_eq"%(save_adress,name_exo,np.amin(T_iso_array),np.amax(T_iso_array),beta,P_tau/(1.e+5))
+            save_name_3D = "%s%s_3D_duo_linear_real_%i_%i_%i_%.2f_eq"%(save_adress,name_exo,np.amin(T_iso_array),np.amax(T_iso_array),beta,P_tau/(1.e+5))
 
     np.save("%s%s/%s/%s_data_convert_%i%i%i.npy"%(path,name_file,param_file,name_exo,reso_alt,reso_long,reso_lat),\
                 data_convert)
